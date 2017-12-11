be9ebfae66b363efee90d2a6f5f671f630c575fc
Rey Cruz <reycruz@Reys-MacBook-Pro.local>
2017-12-10 22:00:13 -0800

    pseudo coded

---
+++ game.js
@@ -0,0 +1,8 @@
+// game.js will contain the logic of the homework that will be able to run on the terminal. 
+
+// it should have the logic for the basic hangman game 
+// it should have the logic for the wrong guesses 
+// logic for the correct gueeses 
+// logic for resetting the game 
+// logic for starting the game 
+// guess remaining 
